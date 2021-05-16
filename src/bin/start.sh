14fbabc533cedbefc529e4427fbc0fe6492c25b9
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-16 17:06:45 +0200

    Restructure folders

--- src/bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-cd "$(dirname "$0")"
-pwd
-ls -la ..
-node --experimental-json-modules ../cli.js $1 $2
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+cd "$(dirname "$0")"
+pwd
+ls -la
+ls -la lib/
+node --experimental-json-modules ./cli.js $1 $2
