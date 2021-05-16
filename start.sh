d4840a9029d2d85e22263ca14622b5badc65da31
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-16 17:52:05 +0200

    Debug directory path

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ ls -la ..
 ls -la ../bin
 ls -la ../lib
 ls -la ../lib/node_modules
-ls -la ../lib/node_modules @sasoria
+ls -la ../lib/node_modules/@sasoria
 node --experimental-json-modules ./cli.js $1 $2
