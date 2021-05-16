bf27c2ed7453f036f8af714a8a8a593f6621a699
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-16 20:06:26 +0200

    Use folder for binaries

---
+++ bin/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+cd "$(dirname "$0")"
+node --experimental-json-modules ../lib/node_modules/@sasoria/housing-prices/cli.js $1 $2
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-cd "$(dirname "$0")"
-node --experimental-json-modules ../lib/node_modules/@sasoria/housing-prices/cli.js $1 $2 
