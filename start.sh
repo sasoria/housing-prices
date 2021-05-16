ce740c48d25ebf35d579d42a0fb723911008e88e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-16 19:25:15 +0200

    Fix directory path

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
-cd "$(dirname "$0")"
+dir="$(cd $(dirname "$0"); pwd)"
+
 pwd
-ls -la
-ls -la ..
-ls -la ../bin
-ls -la ../lib
-ls -la ../lib/node_modules
-ls -la ../lib/node_modules/@sasoria/housing-prices
-node --experimental-json-modules ./cli.js $1 $2
+# ls -la
+# ls -la ..
+# ls -la ../bin
+# ls -la ../lib
+# ls -la ../lib/node_modules
+# ls -la ../lib/node_modules/@sasoria/housing-prices
+
+node --experimental-json-modules "$dir/cli.js" $1 $2
