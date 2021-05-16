1516c25b9084aa2b0201e7e2ef478cb5980c70a9
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-16 17:58:34 +0200

    Debug directory path

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 cd "$(dirname "$0")"
 pwd
+ls -la
 ls -la ..
 ls -la ../bin
 ls -la ../lib
 ls -la ../lib/node_modules
-ls -la ../lib/node_modules/@sasoria
+ls -la ../lib/node_modules/@sasoria/housing-prices
 node --experimental-json-modules ./cli.js $1 $2
