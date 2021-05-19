844bddc772eb41b756eb1d7d4b899a388e1ad65d
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-05-19 22:41:25 +0200

    Refactor code

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import parser from "node-html-parser";
 import fetchPrices from "./lib/api.js";
 import locations from "./lib/locations.js";
 import { byPrice, toInteger, byAscendingPrice } from "./lib/price.js";
-import { toReadableFormat, toPrice, toConsole } from "./lib/price.js";
+import { toReadableFormat, toPrices, toConsole } from "./lib/price.js";
 
 const app = async (flags) => {
-  const locationCode = locations.get(flags.location.toLowerCase());
+  const locationCode = locations.get(flags.location);
   const url = `https://www.finn.no/realestate/homes/search.html?location=${locationCode}&sort=PUBLISHED_DESC`;
   const root = parser.parse(await fetchPrices(url));
 
   const prices = root
     .querySelectorAll(".ads__unit__content__list")
     .filter(byPrice)
-    .map(toPrice)
+    .map(toPrices)
     .map(toInteger);
 
   prices.sort(byAscendingPrice).map(toReadableFormat).map(toConsole);
