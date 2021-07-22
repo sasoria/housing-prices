9d1d3e4787578264fad1f1fa8079c62141b4d3e4
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-07-22 20:46:11 +0200

    Seperate constants

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import parser from "node-html-parser";
 import fetchPrices from "./lib/api.js";
 import locations from "./lib/locations.js";
+import { CONTENT_SELECTOR, DETAILS_SELECTOR } from "./lib/constants.js";
+import { PRICE_SELECTOR } from "./lib/constants.js";
 import { byAscendingPrice, parseToIntegers } from "./lib/price.js";
 import { toConsole, toReadableFormat } from "./lib/price.js";
 
@@ -9,21 +11,15 @@ const app = async (flags) => {
   const url = `https://www.finn.no/realestate/homes/search.html?location=${locationCode}&sort=PUBLISHED_DESC`;
   const root = parser.parse(await fetchPrices(url));
 
-  const details = root
-    .querySelectorAll(".ads__unit__content")
-    .map((content) => {
-      const address = content.querySelector(
-        ".ads__unit__content__details"
-      ).textContent;
-      const price = content.querySelector(
-        ".ads__unit__content__list"
-      ).textContent;
+  const details = root.querySelectorAll(CONTENT_SELECTOR).map((content) => {
+    const address = content.querySelector(DETAILS_SELECTOR).textContent;
+    const price = content.querySelector(PRICE_SELECTOR).textContent;
 
-      return {
-        address: address,
-        price: parseToIntegers(price),
-      };
-    });
+    return {
+      address: address,
+      price: parseToIntegers(price),
+    };
+  });
 
   details.sort(byAscendingPrice).map(toReadableFormat).map(toConsole);
 };
